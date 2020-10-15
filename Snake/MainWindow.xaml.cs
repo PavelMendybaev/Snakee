a31668ee29cfb2515af08f824d6ab9882c491d20
Pavel <mendubaev2003@mail.ru>
2020-10-15 22:48:53 +0300

    добавил яблако

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -21,11 +21,16 @@ namespace Snake
 
     public partial class MainWindow : Window
     {
-        int move = 0;
+
         int speedx = 3;
         int speedy = 0;
         int posx = 250;
         int posy = 250;
+
+
+        int appleposx = 250;
+        int appleposy = 250;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,12 +44,28 @@ namespace Snake
         void timer_tick( object sender, EventArgs e)
         {
           
-            Canvas.SetLeft(alement , posx);
-            Canvas.SetTop(alement, posy);
+            Canvas.SetLeft(snake , posx);
+            Canvas.SetTop(snake, posy);
             posx += speedx;
             posy += speedy;
-            
-            
+
+
+            Canvas.SetLeft(apple, appleposx);
+            Canvas.SetTop(apple, appleposy);
+
+            int distx = posx - appleposx;
+            int disty = posy - appleposy;
+
+            scoretext.Content = distx + "  " + disty; 
+            if(distx < 15 & distx > -15 & disty < 15 & disty > -15)
+            {
+                Random rndx = new Random();
+                Random rndy = new Random();
+
+                appleposx = rndx.Next(20, 450);
+                appleposy = rndy.Next(20, 450);
+
+            }
         }
         private void Keydown(object sender,KeyEventArgs  e)
         {
